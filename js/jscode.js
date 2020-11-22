192168d443c92bd864af8ff129a69dd35fb21351
James Serendip <james.serendip@flemingcollege.ca>
2020-11-22 11:20:13 -0800

    jstest

--- js/jscode.js
+++ js/jscode.js
@@ -1,13 +1,15 @@
 
-function correctalert() {
-	alert("After geocorrecting this old satellite image can be integrated with correct ground coordinates");
-	}
-
 
 
 function newimage()  {
 	document.getElementById("satimage").src="images/ricelakecorrected.JPG";
-	correctalert();
+/*	function correcttext() {
+		document.getElementById("corrected").innerHTML = "After geocorrecting this old satellite image can be integrated with correct ground coordinates";
+		setTimeout(function()  {
+			$('#corrected').html('');
+			}, 3000);
+		}
+		correctalert();  */
 	}
 	
 function oldimage()  {
