71e34bcfa97f319fabc60e9ac4520c958db060c8
James Serendip <james.serendip@flemingcollege.ca>
2020-12-06 17:12:08 -0800

    top button

--- js/jscode.js
+++ js/jscode.js
@@ -15,3 +15,24 @@ function oldsat()  {
 
 /* function to show fun facts on button click  
 from w3schools example*/
+
+
+//Get return to top of screen button (adapted from w3schools):
+mybutton = document.getElementById("topreturnBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
